56ff7fdadb3784dacb2630c7fc13b7f7085b0252
codecameo <codecameo92@gmail.com>
2018-05-05 14:24:47 +0600

    Ransom Note

---
+++ HackerRank/RansomNote/RansomNote.cpp
@@ -0,0 +1,56 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+map<string, int> wordMap;
+
+bool ransom_note(vector<string> magazine, vector<string> ransom) {
+    int len = ransom.size();
+    for(int i=0;i<len;i++){
+        if(!wordMap[ransom[i]]) return false;
+        else{
+            wordMap[ransom[i]]--;
+        }
+    }
+    return true;
+}
+
+int main(){
+    int m;
+    int n;
+    cin >> m >> n;
+    vector<string> magazine(m);
+    for(int magazine_i = 0;magazine_i < m;magazine_i++){
+       cin >> magazine[magazine_i];
+        wordMap[magazine[magazine_i]]++;
+    }
+    vector<string> ransom(n);
+    for(int ransom_i = 0;ransom_i < n;ransom_i++){
+       cin >> ransom[ransom_i];
+    }
+    if(ransom_note(magazine, ransom))
+        cout << "Yes\n";
+    else
+        cout << "No\n";
+    return 0;
+}
